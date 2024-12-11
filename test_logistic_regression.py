641dcf56ceb05f710390d0524b8391f8cbe112b5
Boulogne Sasha <105077278+FastaLaPasta@users.noreply.github.com>
2024-12-11 18:41:00 +0100

    gradient descent base

--- test_logistic_regression.py
+++ test_logistic_regression.py
@@ -8,7 +8,7 @@ def get_data():
     return data
 
 
-def sigmazoid(z):
+def sigmoid(z):
     return 1/(1 + np.exp(-z))
 
 
@@ -22,8 +22,27 @@ def cost_function(y, y_pred):
     return cost
 
 
+def gradient_descent(X, y, parameters, L, Ir):
+    N = X.shape[0]
+    for iteration in range(Ir):
+        z = np.dot(X, parameters)
+        prediction = sigmoid(z)
+
+        error = prediction - y
+
+        gradient = np.dot(X.T, error) / N
+        
+        parameters -= L * gradient
+    return parameters
+            
+
+
 def main():
+    learning_rates = 0.01
+    iteration_rate = 1000
     data = get_data()
+
+    
     print(data)
 
 
